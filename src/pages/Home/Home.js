5b011e89ad44be83685544666115707bdf23d779
msimic7 <simicmilan017@gmail.com>
2020-07-17 11:35:06 +0200

    Add Home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./Home.module.scss";
+import Nav from "../../components/Nav";
 
 const Home = () => {
-    return <div>Home</div>;
+    return (
+        <div className={styles.home}>
+            <Nav />
+            <div className={styles.ordernow}>
+                <div className={styles.ordernow__content}>
+                    <h1>YummiPizza</h1>
+                    <p>Fastest delivery in town!</p>
+                    <Link to="/menu">
+                        <button>ORDER NOW</button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
