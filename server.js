2ffbc080610ac3826f4803edd989ea3560727ef1
msimic7 <simicmilan017@gmail.com>
2020-07-20 11:44:01 +0200

    React router Heroku fix

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + "/"));
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("build"));
     app.get("*", (req, res) => {
-        res.sendFile(path.join("build", "index.html"));
+        res.sendFile(path.join((__dirname, "./build/index.html")));
     });
 }
 
